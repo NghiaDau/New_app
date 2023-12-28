ac795ebba1a9b2d3bce183bce71518f3a66839e0
VanNghia <nghiadauvan@gmail.com>
2023-12-28 19:45:44 +0700

    event-state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-
+import {useState} from "react"
 import './App.css';
 
 function App() {
   const onclick = ()=>{
     console.log("Nghia");
   }
-  let number = 0;
+  let [number,setNumber] = useState(0);
   const onchange = () =>{
     number++;
     console.log(number);
   }
-  const n = "Nghia";
-  const onchange1 = (number) =>{
+  const Increment = () =>{
+    setNumber(number+1);
+    console.log(number);
+  }
+  const onchange1 = (setNumber) =>{
+    setNumber(number-1);
     console.log(number);
   }
   return (
     <div className="App">
+      <h1>{number}</h1>
       <button onClick={onclick}>Nhan me</button>
       <input onChange={onchange}></input>
-      <button onClick={()=> {onchange1(n)}}>giam</button>
+      <button onClick={()=> {onchange1(setNumber)}}>giam</button>
+      <button onClick={Increment}>tang</button>
     </div>
   );
 }
